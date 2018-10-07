a046cc543eb1b08e5c39015b90fd0c93d53249ae
Victoriia Holub <mikimiki158@gmail.com>
2018-10-07 23:59:15 +0300

    added task2 to first lesson

---
+++ Task2.py
@@ -0,0 +1,39 @@
+'''
+The script that shows different arithmetic operators with variables a and b.
+'''
+
+print("Addition + : ")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("The result of " + str(a) + " + " + str(b) + " =",  a + b)
+
+print("Subtraction - :")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("The result of " + str(a) + " - " + str(b) + " =",  a - b)
+
+print("Multiplication * :")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("The result of " + str(a) + " * " + str(b) + " =",  a * b)
+
+print("Exponent ** :")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("The result of " + str(a) + " ** " + str(b) + " =",  a ** b)
+
+print("Division / :")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("The result of " + str(a) + " / " + str(b) + " =",  a / b)
+
+print("Integer Division // :")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("The result of " + str(a) + " // " + str(b) + " =",  a // b)
+
+print("Modulus % :")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("The result of " + str(a) + " % " + str(b) + " =",  a % b)
+
